0898f53ea45bee19a3f5d173b51bee0a3c121e81
DeveloperAspire <franklinokolie86@gmail.com>
2022-06-01 17:14:39 +0100

    Finished CSS styling

--- src/init.js
+++ src/init.js
@@ -1,5 +1,24 @@
-const init = () => {
-    console.log("Hello");
-};
+import game from "./gameState";
 
-export default init;
+const TICK_RATE = 3000;
+
+async function init() {
+  console.log("Starting Game");
+
+  let nextTimeToTick = Date.now();
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      game.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  requestAnimationFrame(nextAnimationFrame);
+}
+
+//init();
